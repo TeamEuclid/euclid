877e54b3f3eac63b4fe8d82582ef9910a0fe14f2
aas <aas@aas.(none)>
2011-10-21 14:55:53 -0700

    edited c++ hello world to also print Aaron Skomra

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
 
   cout << "Hello World" << endl;   //Print out Hello World
+  cout << "Aaron Skomra" << end;   //Edit Hello World to print out your name
   return 0;
 
 }
