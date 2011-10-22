e195787a33dfb76500899c97b92b56998dd341b8
David R Snyder <dsnyder@ruby.cat.pdx.edu>
2011-10-22 14:03:09 -0700

    Added a line that prints my name, and added a missing "l" to endl in the previous print statement.

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main() {
 
   cout << "Hello World" << endl;   //Print out Hello World
-  cout << "Aaron Skomra" << end;   //Edit Hello World to print out your name
+  cout << "Aaron Skomra" << endl;   //Edit Hello World to print out your name
+  cout << "David Snyder" << endl;
   return 0;
 
 }
