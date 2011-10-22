763d01438b03e26435bea62c1e6c55c271a91a26
David R Snyder <dsnyder@ruby.cat.pdx.edu>
2011-10-22 14:13:22 -0700

    edited helloworld.cpp to print my name

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,6 +5,7 @@ int main() {
 
   cout << "Hello World" << endl;   //Print out Hello World
   cout << "Aaron Skomra" << endl;   //Edit Hello World to print out your name
+  //Printing "David Snyder" to the command line
   cout << "David Snyder" << endl;
   return 0;
 
