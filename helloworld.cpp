990a8bc5ec3cdfe644c40262504163b5bd75b0bc
Jess VanDerwalker <washu@sonic.net>
2011-10-24 12:58:04 -0700

    Added my name to the printout.

--- helloworld.cpp
+++ helloworld.cpp
@@ -9,11 +9,6 @@ int main() {
   cout << "David Snyder" << endl;
   cout << "Ben Huddle" << endl;     // Printing out Ben Huddle
   cout << "CARRRRRRRRBSSS" << endl;     // Print: CARRRRRRRRBSSS
-
-  // Okay, this one really is on the head
-  cout << "Here is another change" << endl;
-
-  // This is a change I've made
   cout << "Jess VanDerwalker" << endl;
   return 0;
 
