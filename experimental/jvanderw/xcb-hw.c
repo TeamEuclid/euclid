84e002473add776499189a0bc22c483679ce62a4
Jess VanDerwalker <washu@sonic.net>
2011-11-13 12:47:22 -0800

    Initial commit of experimental code to learn the XCB library.

---
+++ experimental/jvanderw/xcb-hw.c
@@ -0,0 +1,18 @@
+/* Copyright (c) 2011 Jess VanDerwalker
+ *
+ * Test program to create a window using the XCB library.
+ */
+
+#include <unistd.h>
+#include <xcb/xcb.h>
+
+int main (int argc, char *argv) {
+
+    xcb_connection_t    *connection;
+    xcb_screen_t        *screen;
+    xcb_window_t        win;
+
+    pause();
+
+    return 0;
+}
