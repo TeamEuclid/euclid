718849a3438a6513b285c845cdf4a49ea3f63db6
Jess VanDerwalker <washu@sonic.net>
2011-12-02 13:20:22 -0800

    Initial commit of driver for Obj-C code.

---
+++ experimental/jvanderw/obj-c/main.m
@@ -0,0 +1,22 @@
+// Copyright (c) JessVanDerwalker
+//
+// Driver for ModUtil
+//
+
+#import <stdio.h>
+#import "modutil.h"
+
+int main(int argc, char **argv) {
+
+  ModUtil *mod = [[ModUtil alloc] init];
+
+  // Set initial values
+  [mod setValueModPower: 5 andSetMod: 3 andSetPower: 1];
+  
+  printf("The modulus is: %d\n", [mod getMod]);
+
+  // Free any memory
+  [mod release];
+
+  return 0;
+}
