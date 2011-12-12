1add4c118a9fac5bda051fa664884dc335f46c78
Jess VanDerwalker <washu@sonic.net>
2011-12-11 22:38:14 -0800

    Added function def for reparenting windows.

--- experimental/jvanderw/lp-xcb/lpxcb_api.h
+++ experimental/jvanderw/lp-xcb/lpxcb_api.h
@@ -5,12 +5,16 @@
 
 #include <xcb/xcb.h>
 
-typedef struct (xcb_connection_t *conn, xcb_window_t window)
+
+typedef struct
 {
-    xcb_connection_t conn;
+    xcb_connection_t *conn;
     xcb_window_t window;
 } lpxcb_window;
 
+
+void
+reparent_existing_windows(xcb_connection_t *conn, xcb_window_t root);
 
 lpxcb_window *
 lpxcb_find_window (xcb_connection_t *conn, xcb_window_t window);
