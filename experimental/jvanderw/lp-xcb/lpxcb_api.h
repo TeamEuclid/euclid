8ac011b57da1b84863c9780ca653f9ff5214a5fa
Jess VanDerwalker <washu@sonic.net>
2011-12-15 12:01:20 -0800

    Removed function definitions. Only holds a data type now.

--- experimental/jvanderw/lp-xcb/lpxcb_api.h
+++ experimental/jvanderw/lp-xcb/lpxcb_api.h
@@ -3,21 +3,30 @@
  * lpxcb.h
  */
 
+#ifndef _LPXCB_API_H_
+#define _LPXCB_API_H_
+
+#include <stdlib.h>
+#include <stdio.h>
 #include <xcb/xcb.h>
+#include <xcb/damage.h>
+#include <xcb/composite.h>
 
 
-typedef struct
-{
+/* Holds information for a window */
+typedef struct lpxcb_window_t {
+    /* The connection associated with this window */
     xcb_connection_t *conn;
-    xcb_window_t window;
-} lpxcb_window;
+    /* The id of this window */
+    xcb_drawable_t window;
+    /* The id of this parent's window, if it has one. */
+    xcb_drawable_t parent;
+    /* Damage associated with this window */
+    xcb_damage_damage_t damage;
+
+    /* Should we have something for the pixmap? */
+} lpxcb_window_t;
 
 
-void
-reparent_existing_windows(xcb_connection_t *conn, xcb_window_t root);
 
-lpxcb_window *
-lpxcb_find_window (xcb_connection_t *conn, xcb_window_t window);
-
-lpxcb_window *
-lpxcb_add_window (xcb_connection_t *conn, xcb_window_t window);
+#endif  /* _LPXCB_API_H_ */
