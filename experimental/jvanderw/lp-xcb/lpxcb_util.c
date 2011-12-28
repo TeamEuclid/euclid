d87d773e929a8974897dd7dc845eb575aba61e6a
Jess VanDerwalker <washu@sonic.net>
2011-12-28 13:00:37 -0800

    First pass on implementation of utility functions.

---
+++ experimental/jvanderw/lp-xcb/lpxcb_util.c
@@ -0,0 +1,54 @@
+/* Copyright (c) 2011 Jess VanDerwalker
+ *
+ * lpxcb_util.c
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include "lpxcb_util.h"
+
+int
+lpxcb_check_request (xcb_connection_t *conn, xcb_void_cookie_t cookie,
+                     char *msg)
+{
+    xcb_generic_error_t *error;
+
+    error = xcb_request_check(conn, cookie);
+    if (error) {
+        if (msg) {
+            fprintf(stderr, "ERROR: ");
+            fprintf(stderr, msg);
+            fprintf(stderr, "\nError code: %d\n", error->error_code);
+        }
+        return error->error_code;
+    }
+    return 0;
+}
+
+xcb_get_geometry_reply_t *
+lpxcb_get_window_geometry (xcb_connection_t *conn, xcb_window_t window)
+{
+    xcb_get_geometry_cookie_t cookie;
+
+
+    cookie = xcb_get_geometry(conn, window);
+    return xcb_get_geometry_reply(conn, cookie, NULL);
+}
+
+void
+lpxcb_set_window_dimensions (xcb_connection_t *conn, xcb_window_t window,
+                             uint16_t x, uint16_t y,
+                             uint16_t height, uint16_t width,
+                             uint16_t border_width)
+{
+    uint32_t values[] = {x , y, height, width, border_width};
+    xcb_void_cookie_t cookie;
+
+    cookie = xcb_configure_window_checked(conn, window,
+                                          XCB_CONFIG_WINDOW_X |
+                                          XCB_CONFIG_WINDOW_Y |
+                                          XCB_CONFIG_WINDOW_WIDTH | 
+                                          XCB_CONFIG_WINDOW_HEIGHT,
+                                          values);
+    lpxcb_check_request(conn, cookie, "Failed to set new window position and dimentions");
+}
