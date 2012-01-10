2519b290e0fc5b53896d1675e2d9f8dfed2d1591
Jess VanDerwalker <washu@sonic.net>
2012-01-10 10:38:50 -0800

    Initial commit of rootimg.c work.

---
+++ experimental/jvanderw/rootimg/rootimg.c
@@ -0,0 +1,15 @@
+/* Copyright (c) 2012 Jess VanDerwalker
+ *
+ * rootimg.c
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <xcb/xcb.h>
+#include <xcb/xcb_image.h>
+
+int
+main (int argc, char **argv)
+{
+    return 0;
+}
