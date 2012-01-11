23fdad02a0be2e39a92c9010d9807fdb00855654
Jess VanDerwalker <washu@sonic.net>
2012-01-11 13:17:13 -0800

    Started work on handing keyboard event.

--- experimental/jvanderw/rootimg/rootimg.c
+++ experimental/jvanderw/rootimg/rootimg.c
@@ -50,6 +50,9 @@ main (int argc, char **argv)
     xcb_drawable_t root_window;
     xcb_drawable_t window;
 
+    uint32_t mask;
+    uint32_t values[1];
+
     xcb_void_cookie_t cookie;
 
     xcb_get_geometry_reply_t *geom_reply;
@@ -76,6 +79,10 @@ main (int argc, char **argv)
                           (unsigned int) ~0L,
                           XCB_IMAGE_FORMAT_Z_PIXMAP);
 
+    /* Set up the events the window will recognize */
+    mask = XCB_CW_EVENT_MASK;
+    values[0] = XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_KEY_PRESS;
+
     /* Create our new window. Make it half the size */
     window = xcb_generate_id(conn);
     cookie = xcb_create_window_checked(conn,
@@ -89,8 +96,8 @@ main (int argc, char **argv)
                                        geom_reply->border_width,
                                        XCB_WINDOW_CLASS_INPUT_OUTPUT,
                                        root_screen->root_visual,
-                                       0,
-                                       0);
+                                       mask,
+                                       values);
     if (RequestCheck(conn, cookie, "Falied to create new window")) {
         exit(1);
     }
@@ -144,8 +151,16 @@ main (int argc, char **argv)
     xcb_flush(conn);
 
     /* Enter infinte loop so the window stays open */
-    while (1) {
-        /* This is where we would handle events */
+    while ((event = xcb_wait_for_event(conn))) {
+        switch (event->response_type & ~0x80) {
+        case XCB_KEY_PRESS: {
+            xcb_key_press_event_t *kpevent = (xcb_key_press_event_t *) event;
+            printf("A key was pressed\n");
+        }
+        default: {
+            break;
+        }
+        }
     }
 
     /* Never get here, but if we could, would still want to clean up memory */
