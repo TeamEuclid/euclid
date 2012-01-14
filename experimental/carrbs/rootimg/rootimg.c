e49f1404966bba49fc9d2251bbf7ded46fbe7b80
Ben Sanabria Carr <carrbs@cs.pdx.edu>
2012-01-13 17:06:29 -0800

    messed with rootimg.c some more, trying to get xquartz and xorg servers working simultaneously

--- experimental/carrbs/rootimg/rootimg.c
+++ experimental/carrbs/rootimg/rootimg.c
@@ -15,16 +15,19 @@ int
 RequestCheck (xcb_connection_t *conn, xcb_void_cookie_t cookie,
                      char *msg)
 {
-    xcb_generic_error_t *error;
+    xcb_generic_error_t *err;
 
-    error = xcb_request_check(conn, cookie);
-    if (error) {
+    err = xcb_request_check(conn, cookie);
+    if (err) {
         if (msg) {
             fprintf(stderr, "ERROR: ");
-            //fprintf(stderr, msg);
-            fprintf(stderr, "\nError code: %d\n", error->error_code);
+            fprintf(stderr, "%s\n",msg);
+            printf("ERROR: type: %d, error: %d, major: %d, minor: %d, res:%d\n",
+                     err->response_type, err->error_code,err->major_code,
+                     err->minor_code,err->resource_id);
+            //fprintf(stderr, "Error code: %d\n", error->error_code);
         }
-        return error->error_code;
+        return err->error_code;
     }
     return 0;
 }
@@ -44,8 +47,9 @@ GetWindowGeometry (xcb_connection_t *conn, xcb_window_t window)
 int
 main (int argc, char **argv)
 {
-    xcb_connection_t *conn;
     int conn_screen;
+    xcb_connection_t *from = xcb_connect(argv[1], &conn_screen);
+    xcb_connection_t *to = xcb_connect(NULL, NULL);
     xcb_screen_t *root_screen;
     xcb_drawable_t root_window;
     xcb_drawable_t window;
@@ -62,15 +66,15 @@ main (int argc, char **argv)
     xcb_gcontext_t gc;
 
     /* By using NULL as first argument, the value of $DISPLAY is used */
-    conn = xcb_connect(NULL, &conn_screen);
-    root_screen = xcb_aux_get_screen(conn, conn_screen);
+    // conn = xcb_connect(NULL, &conn_screen); // this is already set.
+    root_screen = xcb_aux_get_screen(from, conn_screen);
     root_window = root_screen->root;
 
     /* Get the geometry of the root window */
-    geom_reply = GetWindowGeometry(conn, root_window);
+    geom_reply = GetWindowGeometry(from, root_window);
 
     /* Get the image of the root window */
-    image = xcb_image_get(conn,
+    image = xcb_image_get(from,
                           root_window,
                           geom_reply->x,
                           geom_reply->y,
@@ -84,8 +88,8 @@ main (int argc, char **argv)
     values[0] = XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_KEY_PRESS;
 
     /* Create our new window. Make it half the size */
-    window = xcb_generate_id(conn);
-    cookie = xcb_create_window_checked(conn,
+    window = xcb_generate_id(from);
+    cookie = xcb_create_window_checked(from,
                                        XCB_COPY_FROM_PARENT,
                                        window,
                                        root_window,
@@ -98,43 +102,43 @@ main (int argc, char **argv)
                                        root_screen->root_visual,
                                        mask,
                                        values);
-    if (RequestCheck(conn, cookie, "Falied to create new window")) {
+    if (RequestCheck(from, cookie, "Failed to create new window")) {
         exit(1);
     }
 
     /* Map the window and flush the connection so it draws to the screen */
-    xcb_map_window(conn, window);
-    xcb_flush(conn);
+    xcb_map_window(to, window);
+    xcb_flush(to);
 
     /* Create the pixmap and associate it with our new window. */
-    pixmap = xcb_generate_id(conn);
-    cookie = xcb_create_pixmap(conn,
+    pixmap = xcb_generate_id(to);
+    cookie = xcb_create_pixmap(to,
                                geom_reply->depth,
                                pixmap,
                                window,
                                geom_reply->width,
                                geom_reply->height);
-    if (RequestCheck(conn, cookie, "Failed to create pixmap")) {
+    if (RequestCheck(to, cookie, "Failed to create pixmap")) {
         exit(1);
     }
 
     /* Put the root_window image into the pixmap. Note that a gc is
      * created, but I believe it is ignored. */
-    gc = xcb_generate_id(conn);
-    xcb_create_gc(conn, gc, window, 0, 0);
-    cookie = xcb_image_put(conn,
+    gc = xcb_generate_id(to);
+    xcb_create_gc(to, gc, window, 0, 0);
+    cookie = xcb_image_put(to,
                            pixmap,
                            gc,
                            image,
                            0,
                            0,
                            0);
-    if (RequestCheck(conn, cookie, "Failed to put image into pixmap")) {
+    if (RequestCheck(to, cookie, "Failed to put image into pixmap")) {
         exit(1);
     }
 
     /* Copy the pixmap into the new window */
-    cookie = xcb_copy_area(conn,
+    cookie = xcb_copy_area(to,
                            pixmap,
                            window,
                            gc,
@@ -144,11 +148,12 @@ main (int argc, char **argv)
                            0,
                            geom_reply->width,
                            geom_reply->height);
-    if (RequestCheck(conn, cookie, "Failed to put image into pixmap")) {
+    if (RequestCheck(to, cookie, "Failed to put image into pixmap")) {
         exit(1);
     }
 
-    xcb_flush(conn);
+    xcb_flush(to);
+    printf("hi!\n");
     while(1);
 /*
      //Enter infinte loop so the window stays open 
@@ -166,7 +171,8 @@ main (int argc, char **argv)
 */
     /* Never get here, but if we could, would still want to clean up memory */
     free(geom_reply);
-    xcb_disconnect(conn);
+    xcb_disconnect(to);
+    xcb_disconnect(from);
 
     return 0;
 }
