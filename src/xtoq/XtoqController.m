1a65968179dc572e0af2c0d4d83abb4a124a708d
Benjamin Sanabria Carr <carrbs@cs.pdx.edu>
2012-03-03 14:34:28 -0800

    commented out the xtoq_request_close function in XtoqController.m

--- src/xtoq/XtoqController.m
+++ src/xtoq/XtoqController.m
@@ -390,7 +390,7 @@
     //use dispatch_async() to handle the actual close 
       dispatch_async(xtoqDispatchQueue, ^{
           NSLog(@"Call xtoq_request_close(theContext)");
-          xtoq_request_close(theContext);
+          //xtoq_request_close(theContext);
       });
 }
 
